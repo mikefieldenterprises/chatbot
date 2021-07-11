b9cea9653e2827b21108fbefbdbb25137b43fdec
Mike Field <mike.field@appnovation.com>
2021-07-11 11:40:56 -0700

    Sets bash interpreter back to the correct value for AWS instead of local development

--- reports/gettranscripts.py
+++ reports/gettranscripts.py
@@ -1,5 +1,5 @@
-#!/usr/local/bin/python3
-#/usr/bin/python3
+#!/usr/bin/python3
+#/usr/local/bin/python3
 
 # Gets contents of the client-data folder (relative to current script, it's back one level, 
 # then up one level in the /client-data/ folder).
